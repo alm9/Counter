221a8469779b3a3cf3be18f5204cb6242568a26b
andre.machado <4ndre.machado@gmail.com>
2020-08-24 23:45:09 -0300

    created a body with a list in a Column, but it is OVERFLOWING!!! TODO: How to solve?

--- lib/counter.dart
+++ lib/counter.dart
@@ -11,10 +11,34 @@ class _CounterState extends State<Counter> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Contador"),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+      ),
+      body: Column(
+        children: [
+          Text("Text one", style: TextStyle(fontSize: 40)),
+          Text("Text two", style: TextStyle(fontSize: 40)),
+          Text("Text three", style: TextStyle(fontSize: 40)),
+          Text("Text four", style: TextStyle(fontSize: 40)),
+          Text("Text five", style: TextStyle(fontSize: 40)),
+          Text("Text six", style: TextStyle(fontSize: 40)),
+          Text("Text seven", style: TextStyle(fontSize: 40)),
+          Text("Text eight", style: TextStyle(fontSize: 40)),
+          Text("Text nine", style: TextStyle(fontSize: 40)),
+          Text("Text ten", style: TextStyle(fontSize: 40)),
+          Text("Text eleven", style: TextStyle(fontSize: 40)),
+          Text("Text twelve", style: TextStyle(fontSize: 40)),
+          Text("Text thirteen", style: TextStyle(fontSize: 40)),
+          Text("Text fourteen", style: TextStyle(fontSize: 40)),
+          Text("Text fifteen", style: TextStyle(fontSize: 40)),
+          Text("Text sixteen", style: TextStyle(fontSize: 40)),
+          Text("Text seventeen", style: TextStyle(fontSize: 40)),
+          Text("Text eighteen", style: TextStyle(fontSize: 40)),
+          Text("Text nineteen", style: TextStyle(fontSize: 40),)
+        ],
       ),
     );
   }
 }
 
-//instead of Container, used the component Scaffold
 
