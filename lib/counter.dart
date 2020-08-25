b3734c8ca786cdea7f20f130d54903019fb0521d
andre.machado <4ndre.machado@gmail.com>
2020-08-25 00:07:59 -0300

    Using setState and it is updating the value of quantity in the screen! :D

--- lib/counter.dart
+++ lib/counter.dart
@@ -9,13 +9,17 @@ class _CounterState extends State<Counter> {
   var quantity = 0;
 
   increment(){
+    setState(() {
     quantity++;
-    debugPrint(quantity.toString());
+    });
+//    debugPrint(quantity.toString());
   }
 
   decrement(){
+    setState(() {
     quantity--;
-    debugPrint(quantity.toString());
+    });
+//    debugPrint(quantity.toString());
   }
 
   @override
@@ -50,25 +54,25 @@ class _CounterState extends State<Counter> {
               ],
             ),
           ),
-          Text("Text one", style: TextStyle(fontSize: 40)),
-          Text("Text two", style: TextStyle(fontSize: 40)),
-          Text("Text three", style: TextStyle(fontSize: 40)),
-          Text("Text four", style: TextStyle(fontSize: 40)),
-          Text("Text five", style: TextStyle(fontSize: 40)),
-          Text("Text six", style: TextStyle(fontSize: 40)),
-          Text("Text seven", style: TextStyle(fontSize: 40)),
-          Text("Text eight", style: TextStyle(fontSize: 40)),
-          Text("Text nine", style: TextStyle(fontSize: 40)),
-          Text("Text ten", style: TextStyle(fontSize: 40)),
-          Text("Text eleven", style: TextStyle(fontSize: 40)),
-          Text("Text twelve", style: TextStyle(fontSize: 40)),
-          Text("Text thirteen", style: TextStyle(fontSize: 40)),
-          Text("Text fourteen", style: TextStyle(fontSize: 40)),
-          Text("Text fifteen", style: TextStyle(fontSize: 40)),
-          Text("Text sixteen", style: TextStyle(fontSize: 40)),
-          Text("Text seventeen", style: TextStyle(fontSize: 40)),
-          Text("Text eighteen", style: TextStyle(fontSize: 40)),
-          Text("Text nineteen", style: TextStyle(fontSize: 40),)
+//          Text("Text one", style: TextStyle(fontSize: 40)),
+//          Text("Text two", style: TextStyle(fontSize: 40)),
+//          Text("Text three", style: TextStyle(fontSize: 40)),
+//          Text("Text four", style: TextStyle(fontSize: 40)),
+//          Text("Text five", style: TextStyle(fontSize: 40)),
+//          Text("Text six", style: TextStyle(fontSize: 40)),
+//          Text("Text seven", style: TextStyle(fontSize: 40)),
+//          Text("Text eight", style: TextStyle(fontSize: 40)),
+//          Text("Text nine", style: TextStyle(fontSize: 40)),
+//          Text("Text ten", style: TextStyle(fontSize: 40)),
+//          Text("Text eleven", style: TextStyle(fontSize: 40)),
+//          Text("Text twelve", style: TextStyle(fontSize: 40)),
+//          Text("Text thirteen", style: TextStyle(fontSize: 40)),
+//          Text("Text fourteen", style: TextStyle(fontSize: 40)),
+//          Text("Text fifteen", style: TextStyle(fontSize: 40)),
+//          Text("Text sixteen", style: TextStyle(fontSize: 40)),
+//          Text("Text seventeen", style: TextStyle(fontSize: 40)),
+//          Text("Text eighteen", style: TextStyle(fontSize: 40)),
+//          Text("Text nineteen", style: TextStyle(fontSize: 40),)
         ],
       ),
     );
